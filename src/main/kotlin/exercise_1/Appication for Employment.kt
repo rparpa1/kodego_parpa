1e5cf321edcf4cc994e15060457bb3908b09887f
RYAN PARPA <ryanparpa24@gmail.com>
2022-10-07 20:21:41 +0800

    add Exercise Application for Employment

--- src/main/kotlin/exercise_1/Appication for Employment.kt
+++ src/main/kotlin/exercise_1/Appication for Employment.kt
@@ -1,21 +1,23 @@
 fun main () {
-// Personal Information
+
     var strPositionforApplying = ""
     var strDate = ""
     var strDesiredSalary = ""
-    var strLastname: String = "PARPA"
-    var strFirstname: String = "RYAN"
-    var strMiddle: String = "L."
-    var strAddress: String = "Pawod"
-    var strCity: String = "Bacolod City"
-    var strState: String = "Philippines"
-    var strZipcode: String = "6105"
+
+    // Personal Information
+    var strLastname: String = ""
+    var strFirstname: String = ""
+    var strMiddle: String = ""
+    var strAddress: String = ""
+    var strCity: String = ""
+    var strState: String = ""
+    var strZipcode: String = ""
     var strHomeNumber: String = ""
-    var strcellphone: String = "+639504715462"
-    var strEmailaddress: String = "ryanparpa24@gmail.com"
-    var strSocialSecurityNumber: String = "5425487451254"
-    var youAmerican = "No"
-    var beenConvictedofafelony = "No"
+    var strcellphone: String = ""
+    var strEmailaddress: String = ""
+    var strSocialSecurityNumber: String = ""
+    var youAmerican = ""
+    var beenConvictedofafelony = ""
 
 println("Last Name :$strLastname")
     print("Citizen" :$)
