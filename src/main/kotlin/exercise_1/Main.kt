1113cf63cbf39977dd7cd656d449a89647fe5e01
RYAN PARPA <ryanparpa24@gmail.com>
2022-10-07 20:19:03 +0800

    add Exercise Application for Employment

--- src/main/kotlin/exercise_1/Main.kt
+++ src/main/kotlin/exercise_1/Main.kt
@@ -1,4 +1,27 @@
 package exercise_1
 
-class Main {
+fun main () {
+
+    var strPositionforApplying = ""
+    var strDate = ""
+    var strDesiredSalary = ""
+
+    // Personal Information
+    var strLastname: String = "PARPA"
+    var strFirstname: String = "RYAN"
+    var strMiddle: String = "L."
+    var strAddress: String = "Pawod"
+    var strCity: String = "Bacolod City"
+    var strState: String = "Philippines"
+    var strZipcode: String = "6105"
+    var strHomeNumber: String = ""
+    var strcellphone: String = "+639504715462"
+    var strEmailaddress: String = "ryanparpa24@gmail.com"
+    var strSocialSecurityNumber: String = "5425487451254"
+    var youAmerican = "No"
+    var beenConvictedofafelony = "No"
+
+    println("Last Name :$strLastname")
+    print("Citizen" :$)
+
 }
