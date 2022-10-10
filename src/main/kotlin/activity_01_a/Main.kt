f6bf6892126dd00a57ed49d964cfea4dd7f9eec6
RYAN PARPA <ryanparpa24@gmail.com>
2022-10-10 20:03:58 +0800

    adding Information for activity_01_a

---
+++ src/main/kotlin/activity_01_a/Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    var courseName = ""
+    var numberOfStudentsEnrolled = ""
+    var maleEnrolled = ""
+    var femaleEnrolled = ""
+    var studentProgress = ""
+    var activeStudent = ""
+    var notActiveStudent = ""
+    var studentPassed = ""
+    var studentFailed = ""
+
+
+
+
+//    println("courseName = $listOfStudentsEntrolled")
+}
